d7d2021131e340d2dd36a26832d380df4bd1776c
raknas999 <32408560+raknas999@users.noreply.github.com>
2023-03-08 15:17:56 +0530

    Create main.tf

---
+++ wmt-customer-tech-adtech/air/main.tf
@@ -0,0 +1,18 @@
+"bucket-name" = {
+  "write" = [
+    "service-account1",
+    "service-account2"
+    ],
+  "read" = [
+    "service-account1",
+    "service-account2"
+    ]
+}
+"madhu_bucket" = {
+	"writer" = ['sankar.dadi@gmail.com', 'sankar.dadi1@gmail.com'],
+	"reader" = ['sankar.dadi@gmail.com', 'sankar.dadi1@gmail.com']
+}
+"sankar_bucket" = {
+	"writer" = ['sankar.dadi@gmail.com'],
+	"reader" = ['sankar.dadi@gmail.com']
+}
