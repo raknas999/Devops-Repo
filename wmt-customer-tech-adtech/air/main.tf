e73df35853b8d45cef2439d1a3d22df92f0e0825
root <root@3e05e118e1bc>
2023-03-09 10:38:57 +0000

    Added new block for air

--- wmt-customer-tech-adtech/air/main.tf
+++ wmt-customer-tech-adtech/air/main.tf
@@ -28,3 +28,38 @@ module "adtech-wmx-data-resources" {
     }
   }
 }
+module "adtech-wmx-data-resources" {
+  source = "../../modules/gcs-buckets"
+
+  trproductid = 3051
+
+  allBucketPermissions = {
+      "writer" = [
+      ],
+      "reader" = [
+      ]
+  }
+
+  buckets = {
+    "sp-artifacts-dev" = {
+      "writer" = [
+        "group:gcp-adtech-campaigns@walmart.com"
+      ],
+      "reader" = [
+      ]
+    },
+    "wmt-adtech-sp-dev-cluster-acl-storage" = {
+      "writer" = [
+        "group:gcp-adtech-campaigns@walmart.com",
+        "serviceAccount:adtech-wpa@wmt-customer-tech-adtech.iam.gserviceaccount.com"
+      ],
+      "reader" = [
+      ]
+    }
+  }
+"air-test-bucket" = {
+    reader = ["air.read-1", "air.read-2"],
+    writer = ["air.write-1"]
+}
+
+}
