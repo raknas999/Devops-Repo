91808c7885d066a0e6006c34182aa80faa002037
root <root@c9356e82ba68>
2023-03-09 10:57:05 +0000

    Added new block for air

--- wmt-customer-tech-adtech/air/main.tf
+++ wmt-customer-tech-adtech/air/main.tf
@@ -28,3 +28,37 @@ module "adtech-wmx-data-resources" {
     }
   }
 }
+module "adtech-wmx-data-resources" {
+  source = "../../modules/gcs-buckets"
+
+  trproductid = 3051
+
+  allBucketPermissions = {
+      "writer" = [
+      ],
+      "reader" = [
+      ]
+  }
+
+  buckets = {
+    "sp-artifacts-dev" = {
+      "writer" = [
+        "group:gcp-adtech-campaigns@walmart.com"
+      ],
+      "reader" = [
+      ]
+    },
+    "wmt-adtech-sp-dev-cluster-acl-storage" = {
+      "writer" = [
+        "group:gcp-adtech-campaigns@walmart.com",
+        "serviceAccount:adtech-wpa@wmt-customer-tech-adtech.iam.gserviceaccount.com"
+      ],
+      "reader" = [
+      ]
+    }
+  }
+"sankar-test" = {
+    reader = ["sankar.read-1", "sankar.read-2"],
+    writer = ["sankar.write-1"]
+}
+
